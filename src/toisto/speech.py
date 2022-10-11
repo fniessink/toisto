a17f8b821fe0fec048d77b4f28b41c279fea4342
Frank Niessink <frank@niessink.com>
2022-10-12 00:27:07 +0200

    Add decks for furniture and house.

--- src/toisto/speech.py
+++ src/toisto/speech.py
@@ -8,4 +8,5 @@ VOICES = dict(fi="Satu", nl="Xander")
 def say(language: str, text: str) -> None:
     """Say the text in the specified language."""
     voice = VOICES[language]
-    os.system(f"say --voice={voice} --interactive=bold '{text}'")
+    text = text.replace("'", r"\'")
+    os.system(f"say --voice={voice} --interactive=bold {text}")
