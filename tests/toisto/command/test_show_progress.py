f6c26164c5eae0b45de5aaf98b8c6c790060c52b
Frank Niessink <frank@niessink.com>
2022-11-13 23:09:39 +0100

    Show until when a quiz is silenced in the progress table.

--- tests/toisto/command/test_show_progress.py
+++ tests/toisto/command/test_show_progress.py
@@ -1,5 +1,6 @@
 """Unit tests for the show progress command."""
 
+from datetime import datetime, timedelta
 from unittest.mock import patch
 
 from toisto.command import show_progress
@@ -23,3 +24,19 @@ class ShowProgressTest(ToistoTestCase):
         with patch("rich.console.Console.print") as console_print:
             show_progress("fi", [quiz], Progress({str(quiz): dict(count=1)}))
         self.assertEqual("1", list(console_print.call_args[0][0].columns[5].cells)[0])
+
+    def test_quiz_silenced_until_time_in_the_future(self):
+        """Test that if the time until which a quiz is silenced lies in the future, it is shown."""
+        quiz = self.create_quiz("fi", "nl", "Terve", ["Hoi"])
+        silence_until = (datetime.now() + timedelta(days=1)).isoformat(sep=" ", timespec="minutes")
+        with patch("rich.console.Console.print") as console_print:
+            show_progress("fi", [quiz], Progress({str(quiz): dict(count=2, silence_until=silence_until)}))
+        self.assertEqual(silence_until, list(console_print.call_args[0][0].columns[6].cells)[0])
+
+    def test_quiz_silenced_until_time_in_the_past(self):
+        """Test that if the time until which a quiz is silenced lies in the past, it is not shown."""
+        quiz = self.create_quiz("fi", "nl", "Terve", ["Hoi"])
+        silence_until = (datetime.now() - timedelta(days=1)).isoformat(sep=" ", timespec="minutes")
+        with patch("rich.console.Console.print") as console_print:
+            show_progress("fi", [quiz], Progress({str(quiz): dict(count=2, silence_until=silence_until)}))
+        self.assertEqual("", list(console_print.call_args[0][0].columns[6].cells)[0])
