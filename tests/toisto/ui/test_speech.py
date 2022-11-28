54e22bdcb00d5c82cab889e92f27789b5003cef4
Frank Niessink <frank@niessink.com>
2022-11-28 22:37:46 +0100

    Use Google Translate for text-to-speech, but fall back to the MacOS say command if getting the audio from Google Translate fails.

--- tests/toisto/ui/test_speech.py
+++ tests/toisto/ui/test_speech.py
@@ -1,7 +1,9 @@
 """Unit tests for the text to speech."""
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
+
+import gtts
 
 from toisto.ui.speech import say
 
@@ -9,8 +11,17 @@ from toisto.ui.speech import say
 class SayTest(unittest.TestCase):
     """Unit tests for the say function."""
 
+    @patch("gtts.gTTS.save", Mock(side_effect=gtts.tts.gTTSError))
     @patch("os.system")
-    def test_system_call(self, mock_os_system):
+    def test_system_call_say(self, mock_os_system):
         """Test that the say program is called with the correct arguments."""
         say("nl", "Hallo")
         mock_os_system.assert_called_once_with("say --voice=Xander Hallo &")
+
+    @patch("gtts.gTTS.save", Mock())
+    @patch("os.system")
+    def test_system_call_afplay(self, mock_os_system):
+        """Test that the afplay program is called with the correct arguments."""
+        say("nl", "Hallo")
+        mock_os_system.assert_called_once()
+        self.assertTrue(mock_os_system.call_args_list[0][0][0].startswith("afplay "))
