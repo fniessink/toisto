645766e9605a3a363cfdb163a1c432964ec38c6f
Frank Niessink <frank@niessink.com>
2022-12-07 18:18:14 +0100

    Refactor where the quiz types are determined.

--- src/toisto/model/language/label.py
+++ src/toisto/model/language/label.py
@@ -2,8 +2,6 @@
 
 from __future__ import annotations
 
-from typing import cast
-
 
 class Label(str):
     """Class representing labels for concepts."""
@@ -20,14 +18,12 @@ class Label(str):
         """Return the hash of the label, ignoring hints."""
         return hash(self.split(";", maxsplit=1)[0])
 
+    @property
     def spelling_alternatives(self) -> Labels:
         """Extract the spelling alternatives from the label."""
         return tuple(self.__class__(label) for label in self.split(";", maxsplit=1)[0].split("|"))
 
-    def first_spelling_alternative(self) -> Label:
-        """Extract the first spelling alternative from the label."""
-        return self.spelling_alternatives()[0]
-
+    @property
     def hint(self) -> str:
         """Return the label hint, if any."""
         return self.split(";")[1] if ";" in self else ""
@@ -39,4 +35,4 @@ Labels = tuple[Label, ...]
 def label_factory(string: str | list[str]) -> Labels:
     """Instantiate the labels from a string or list of strings."""
     labels = string if isinstance(string, list) else [string]
-    return cast(Labels, tuple(Label(label) for label in labels))
+    return tuple(Label(label) for label in labels)
