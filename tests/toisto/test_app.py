6f6d26711599ef8c0df9065388975e2e18c38f17
Frank Niessink <frank@niessink.com>
2022-12-15 20:09:33 +0100

    100% coverage. (#124)
    * 100% coverage.

---
+++ tests/toisto/test_app.py
@@ -0,0 +1,39 @@
+"""Unit tests for the app."""
+
+import argparse
+import os
+import unittest
+from unittest.mock import Mock, patch
+import sys
+
+from toisto.app import main
+
+
+class AppTest(unittest.TestCase):
+    """Unit tests for the main method."""
+
+    @patch.object(argparse.ArgumentParser, "_print_message")
+    def test_no_arguments(self, write):
+        """Test that the app exits if the user does not provide arguments."""
+        os.environ['COLUMNS'] = "120"  # Fake that the terminal is wide enough.
+        self.assertRaises(SystemExit, main)
+        self.assertEqual(
+            f"usage: {sys.argv[0].rsplit('/', maxsplit=1)[-1]} [-h] [-V] "
+            "[-t {topic}] [-f {topic file}] [-s {option}] {language to practice} {your language} [{command}]\n",
+            write.call_args_list[0][0][0]
+        )
+
+    @patch("builtins.input", Mock(side_effect=[EOFError]))
+    @patch.object(sys, "argv", ["toisto", "fi", "nl"])
+    def test_practice(self):
+        """Test that the practice command can be invoked."""
+        with patch("rich.console.Console.print") as patched_print:
+            main()
+        self.assertTrue(patched_print.call_args_list[0][0][0].startswith("👋 Welcome to [underline]Toisto"))
+
+    @patch.object(sys, "argv", ["toisto", "fi", "nl", "progress"])
+    def test_progress(self):
+        """Test that the progress command can be invoked."""
+        with patch("rich.console.Console.print") as patched_print:
+            main()
+        self.assertTrue(patched_print.call_args_list[0][0][0].title.startswith("Progress"))
--- tests/toisto/ui/test_cli.py
+++ tests/toisto/ui/test_cli.py
@@ -1,5 +1,6 @@
 """Unit tests for the CLI module."""
 
+import argparse
 import unittest
 from unittest.mock import Mock, patch
 
@@ -13,3 +14,9 @@ class ParserTest(unittest.TestCase):
     def test_help(self):
         """Test that a help message is displayed."""
         self.assertRaises(SystemExit, parser.parse_args, ["--help"])
+
+    @patch.object(argparse.ArgumentParser, "_print_message")
+    def test_version(self, write):
+        """Test that the app writes the version number to stdout."""
+        self.assertRaises(SystemExit, parser.parse_args, ["--version"])
+        self.assertEqual('0.4.0\n', write.call_args_list[0][0][0])
