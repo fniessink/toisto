ab79683580aad3534ebfc1df1cc6b7f8ed5a0a2c
Frank Niessink <frank@niessink.com>
2023-01-01 21:15:10 +0100

    Small refactor.

--- src/toisto/model/quiz/progress.py
+++ src/toisto/model/quiz/progress.py
@@ -35,15 +35,17 @@ class Progress:
 
     def __eligible_quizzes(self, topic: Topic, quizzes: Quizzes, must_have_progress: bool) -> Quizzes:
         """Return the eligible next quizzes for the topic if possible."""
-        eligible = set(quiz for quiz in quizzes if self.__is_eligible(quiz))
-        concepts = set(quiz.concept_id for quiz in eligible)
-        eligible = set(quiz for quiz in eligible if not set(quiz.uses) & concepts and quiz in topic.quizzes)
+        eligible = set(quiz for quiz in quizzes if self.__is_eligible(topic, quiz))
+        concepts = set(quiz.concept_id for quiz in quizzes)
+        eligible = set(quiz for quiz in eligible if not set(quiz.uses) & concepts)
         eligible_with_progress = set(quiz for quiz in eligible if self.__has_progress(quiz))
         return easiest_quizzes(eligible_with_progress if must_have_progress else eligible_with_progress or eligible)
 
-    def __is_eligible(self, quiz: Quiz) -> bool:
+    def __is_eligible(self, topic: Topic, quiz: Quiz) -> bool:
         """Is the quiz eligible?"""
-        return not self.get_retention(quiz).is_silenced() and not quiz.has_same_concept(self.__current_quiz)
+        return quiz in topic.quizzes and \
+            not self.get_retention(quiz).is_silenced() and \
+            not quiz.has_same_concept(self.__current_quiz)
 
     def __has_progress(self, quiz: Quiz) -> bool:
         """Has the quiz been presented to the user before?"""
