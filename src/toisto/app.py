55bc5442a1037c186c3faf98a8f5938a1271f344
Frank Niessink <frank@niessink.com>
2023-01-17 18:13:46 +0100

    WIP.

--- src/toisto/app.py
+++ src/toisto/app.py
@@ -1,6 +1,7 @@
 """Main module for the application."""
 
 import readline  # pylint: disable=unused-import
+import requests
 
 from .command import practice, show_topics, show_progress
 from .persistence import load_topics, load_progress
@@ -13,6 +14,10 @@ def main():
     args = argument_parser.parse_args()
     topics = load_topics(args.language, args.source_language, args.topic, args.topic_file)
     progress = load_progress(topics)
+    try:
+        print(requests.get("https://api.github.com/repos/fniessink/toisto/tags").json()[0]["name"])
+    except requests.ConnectionError:
+        pass
     if args.command == "practice":
         show_welcome()
         practice(progress)
