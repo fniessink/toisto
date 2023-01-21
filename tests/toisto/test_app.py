def49d34d1eb51f3af515ff54fee5760b81e01c5
Frank Niessink <frank@niessink.com>
2023-01-21 17:24:11 +0100

    Refactor unit tests. (#177)

--- tests/toisto/test_app.py
+++ tests/toisto/test_app.py
@@ -1,10 +1,8 @@
 """Unit tests for the app."""
 
-import argparse
-import os
 import unittest
-from unittest.mock import Mock, patch
 import sys
+from unittest.mock import Mock, patch
 
 import requests
 
@@ -15,66 +13,62 @@ from toisto.metadata import VERSION
 class AppTest(unittest.TestCase):
     """Unit tests for the main method."""
 
-    @patch.object(argparse.ArgumentParser, "_print_message")
-    def test_no_arguments(self, write):
-        """Test that the app exits if the user does not provide arguments."""
-        os.environ["COLUMNS"] = "120"  # Fake that the terminal is wide enough.
-        self.assertRaises(SystemExit, main)
-        self.assertEqual(
-            f"usage: {sys.argv[0].rsplit('/', maxsplit=1)[-1]} [-h] [-V] {{practice,progress,topics}} ...\n",
-            write.call_args_list[0][0][0],
-        )
+    def setUp(self):
+        """Set up test fixtures."""
+        self.latest_version = Mock(json=Mock(return_value=[dict(name="v9999")]))
 
     @patch("os.system", Mock())
-    @patch("builtins.input", Mock(side_effect=[EOFError]))
-    @patch("requests.get", Mock(return_value=Mock(json=Mock(return_value=[dict(name="v9999")]))))
-    @patch.object(sys, "argv", ["toisto", "practice", "fi", "nl"])
-    def test_practice(self):
-        """Test that the practice command can be invoked."""
+    @patch("builtins.input", Mock(side_effect=EOFError))
+    def run_main(self) -> Mock:
+        """Run the main function and return the patched print method."""
         with patch("rich.console.Console.print") as patched_print:
             main()
+        return patched_print
+
+    @patch.object(sys, "argv", ["toisto", "practice", "fi", "nl"])
+    @patch("requests.get")
+    def test_practice(self, requests_get):
+        """Test that the practice command can be invoked."""
+        requests_get.return_value = self.latest_version
+        patched_print = self.run_main()
         self.assertTrue(patched_print.call_args_list[0][0][0].startswith("👋 Welcome to [underline]Toisto"))
 
-    @patch("os.system", Mock())
-    @patch("builtins.input", Mock(side_effect=[EOFError]))
-    @patch("requests.get", Mock(return_value=Mock(json=Mock(return_value=[dict(name="v9999")]))))
     @patch.object(sys, "argv", ["toisto", "practice", "fi", "nl"])
-    def test_new_version(self):
+    @patch("requests.get")
+    def test_new_version(self, requests_get):
         """Test that the practice command shows a new version."""
-        with patch("rich.console.Console.print") as patched_print:
-            main()
+        requests_get.return_value = self.latest_version
+        patched_print = self.run_main()
         self.assertTrue("v9999" in patched_print.call_args_list[1][0][0].renderable)
 
-    @patch("os.system", Mock())
-    @patch("builtins.input", Mock(side_effect=[EOFError]))
-    @patch("requests.get", Mock(return_value=Mock(json=Mock(return_value=[dict(name=VERSION)]))))
     @patch.object(sys, "argv", ["toisto", "practice", "fi", "nl"])
-    def test_current_version(self):
+    @patch("requests.get")
+    def test_current_version(self, requests_get):
         """Test that the practice command does not show the current version."""
-        with patch("rich.console.Console.print") as patched_print:
-            main()
+        requests_get.return_value = Mock(json=Mock(return_value=[dict(name=VERSION)]))
+        patched_print = self.run_main()
         self.assertTrue("[quiz]" in patched_print.call_args_list[1][0][0])
 
-    @patch("os.system", Mock())
-    @patch("builtins.input", Mock(side_effect=[EOFError]))
-    @patch("requests.get", Mock(side_effect=requests.ConnectionError))
     @patch.object(sys, "argv", ["toisto", "practice", "fi", "nl"])
-    def test_github_connection_error(self):
+    @patch("requests.get")
+    def test_github_connection_error(self, requests_get):
         """Test that the practice command starts even if GitHub cannot be reached to get the latest version."""
-        with patch("rich.console.Console.print") as patched_print:
-            main()
+        requests_get.side_effect = requests.ConnectionError
+        patched_print = self.run_main()
         self.assertTrue(patched_print.call_args_list[0][0][0].startswith("👋 Welcome to [underline]Toisto"))
 
     @patch.object(sys, "argv", ["toisto", "progress", "fi", "nl"])
-    def test_progress(self):
+    @patch("requests.get")
+    def test_progress(self, requests_get):
         """Test that the progress command can be invoked."""
-        with patch("rich.console.Console.print") as patched_print:
-            main()
+        requests_get.return_value = self.latest_version
+        patched_print = self.run_main()
         self.assertTrue(patched_print.call_args_list[0][0][0].title.startswith("Progress"))
 
     @patch.object(sys, "argv", ["toisto", "topics", "fi", "nl"])
-    def test_topics(self):
+    @patch("requests.get")
+    def test_topics(self, requests_get):
         """Test that the topics command can be invoked."""
-        with patch("rich.console.Console.print") as patched_print:
-            main()
+        requests_get.return_value = self.latest_version
+        patched_print = self.run_main()
         self.assertTrue(patched_print.call_args_list[0][0][0].title.startswith("Topic"))
--- tests/toisto/ui/test_cli.py
+++ tests/toisto/ui/test_cli.py
@@ -4,19 +4,29 @@ import argparse
 import unittest
 from unittest.mock import Mock, patch
 
-from toisto.ui.cli import argument_parser
+from toisto.ui.cli import create_argument_parser
 
 
 class ParserTest(unittest.TestCase):
     """Unit tests for the CLI parser."""
 
+    def setUp(self) -> None:
+        """Set up the test fixtures."""
+        with patch("requests.get", Mock(return_value=Mock(json=Mock(return_value=[dict(name="v9999")])))):
+            self.argument_parser = create_argument_parser()
+
     @patch("sys.stdout", Mock())
     def test_help(self):
         """Test that a help message is displayed."""
-        self.assertRaises(SystemExit, argument_parser.parse_args, ["--help"])
+        self.assertRaises(SystemExit, self.argument_parser.parse_args, ["--help"])
+
+    @patch("sys.stderr", Mock())
+    def test_no_args(self):
+        """Test that a help message is displayed if no arguments are supplied."""
+        self.assertRaises(SystemExit, self.argument_parser.parse_args, [])
 
     @patch.object(argparse.ArgumentParser, "_print_message")
     def test_version(self, write):
         """Test that the app writes the version number to stdout."""
-        self.assertRaises(SystemExit, argument_parser.parse_args, ["--version"])
-        self.assertRegex(write.call_args_list[0][0][0], r"\d+.\d+.\d+\n")
+        self.assertRaises(SystemExit, self.argument_parser.parse_args, ["--version"])
+        self.assertRegex(write.call_args_list[0][0][0], r"\d+.\d+.\d+")
