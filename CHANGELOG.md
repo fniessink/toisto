9e14ffc14d891f406ea23b88bad8204dbb26c0ba
Frank Niessink <frank@niessink.com>
2023-02-01 21:14:47 +0100

    Fix spelling error (comparative, not comparitive). (#194)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to Toisto will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Fixed
+
+- Fix spelling error (comparative, not comparitive). Unfortunately, this means that progress on quizzes that ask to give the comparative degree is reset.
+
 ## 0.7.0 - 2023-01-28
 
 ### Fixed
--- src/toisto/model/language/grammar.py
+++ src/toisto/model/language/grammar.py
@@ -5,7 +5,7 @@ from typing import Literal
 GrammaticalGender = Literal["female", "male", "neuter"]
 GrammaticalNumber = Literal["infinitive", "singular", "plural"]
 GrammaticalPerson = Literal["first person", "second person", "third person"]
-DegreeOfComparison = Literal["positive degree", "comparitive degree", "superlative degree"]
+DegreeOfComparison = Literal["positive degree", "comparative degree", "superlative degree"]
 Tense = Literal["present tense", "past tense"]
 SentenceForm = Literal["declarative", "interrogative"]
 GrammaticalPolarity = Literal["affirmative", "negative"]
