4d2347b08530d8ad6737298996f3f8f48a7fd82f
Frank Niessink <frank@niessink.com>
2023-02-03 23:52:07 +0100

    More colorful help information. (#199)

--- src/toisto/ui/cli.py
+++ src/toisto/ui/cli.py
@@ -2,13 +2,15 @@
 
 from argparse import ArgumentParser, _SubParsersAction
 
-from ..metadata import SUMMARY, SUPPORTED_LANGUAGES, TOPICS, VERSION, latest_version
+from rich_argparse import RichHelpFormatter
+
+from ..metadata import HOMEPAGE_URL, SUMMARY, SUPPORTED_LANGUAGES, TOPICS, VERSION, latest_version
 
 
 def add_language_arguments(parser: ArgumentParser) -> None:
     """Add the language arguments to the parser."""
     choices = SUPPORTED_LANGUAGES.keys()
-    target_language_help = "language to practice; available languages: %(choices)s"
+    target_language_help = "target language; available languages: %(choices)s"
     parser.add_argument("target_language", metavar="{target language}", choices=choices, help=target_language_help)
     source_language_help = "source language; available languages: %(choices)s"
     parser.add_argument("source_language", metavar="{source language}", choices=choices, help=source_language_help)
@@ -16,7 +18,7 @@ def add_language_arguments(parser: ArgumentParser) -> None:
 
 def add_topic_arguments(parser: ArgumentParser) -> None:
     """Add the topic arguments to the parser."""
-    topic_help = "topic to use, can be repeated (default: all); available topics: %(choices)s"
+    topic_help = "topic to use, can be repeated; default: all; available topics: %(choices)s"
     parser.add_argument(
         "-t",
         "--topic",
@@ -42,7 +44,7 @@ def add_progress_command(subparser: _SubParsersAction) -> None:
         "show progress, for example `%(prog)s progress fi en` shows progress on practicing Finnish from English"
     )
     parser = add_command(subparser, "progress", "Show progress.", command_help)
-    sort_help = "how to sort progress information (default: by retention); available options: %(choices)s"
+    sort_help = "how to sort progress information; default: by retention; available options: %(choices)s"
     parser.add_argument(
         "-s",
         "--sort",
@@ -56,12 +58,17 @@ def add_progress_command(subparser: _SubParsersAction) -> None:
 def add_topics_command(subparser: _SubParsersAction) -> None:
     """Add a command to show topics."""
     command_help = "show topics, for example `%(prog)s topics -t nature` shows the contents of the nature topic"
-    add_command(subparser, "topics", "Show topics", command_help)
+    add_command(subparser, "topics", "Show topics.", command_help)
 
 
 def add_command(subparser: _SubParsersAction, command: str, description: str, command_help: str) -> ArgumentParser:
     """Add a command."""
-    parser = subparser.add_parser(command, description=description, help=command_help)
+    parser = subparser.add_parser(
+        command,
+        description=description,
+        help=command_help,
+        formatter_class=RichHelpFormatter,
+    )
     add_language_arguments(parser)
     add_topic_arguments(parser)
     return parser
@@ -69,11 +76,12 @@ def add_command(subparser: _SubParsersAction, command: str, description: str, co
 
 def create_argument_parser() -> ArgumentParser:
     """Create the argument parser."""
-    argument_parser = ArgumentParser(description=SUMMARY)
+    epilog = f"See {HOMEPAGE_URL}/blob/main/README.md for more information."
+    argument_parser = ArgumentParser(description=SUMMARY, epilog=epilog, formatter_class=RichHelpFormatter)
     latest = latest_version()
     version = f"v{VERSION}" + (f" ({latest} is available)" if latest and latest.strip("v") > VERSION else "")
     argument_parser.add_argument("-V", "--version", action="version", version=version)
-    command_help = "type `%(prog)s {command} -h` for more information on a command"
+    command_help = "type `%(prog)s {command} --help` for more information on a command"
     subparser_action = argument_parser.add_subparsers(
         dest="command",
         title="commands",
