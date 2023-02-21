f6ccfd8ef7fde1456d2101be2da7a6d3ddd93d2e
Frank Niessink <frank@niessink.com>
2023-02-21 21:44:35 +0100

    Remove magic constants.

--- src/toisto/model/language/label.py
+++ src/toisto/model/language/label.py
@@ -6,9 +6,12 @@ from __future__ import annotations
 class Label(str):
     """Class representing labels for concepts."""
 
+    HINT_SEP = ";"
+    SPELLING_ALTERNATIVES_SEP = "|"
+
     def __eq__(self, other: object) -> bool:
         """Ignore hints when determining equality."""
-        return self.split(";", maxsplit=1)[0] == str(other).split(";", maxsplit=1)[0]
+        return self.split(self.HINT_SEP, maxsplit=1)[0] == str(other).split(self.HINT_SEP, maxsplit=1)[0]
 
     def __ne__(self, other: object) -> bool:
         """Return whether the labels are not equal."""
@@ -16,17 +19,18 @@ class Label(str):
 
     def __hash__(self) -> int:
         """Return the hash of the label, ignoring hints."""
-        return hash(self.split(";", maxsplit=1)[0])
+        return hash(self.split(self.HINT_SEP, maxsplit=1)[0])
 
     @property
     def spelling_alternatives(self) -> Labels:
         """Extract the spelling alternatives from the label."""
-        return tuple(self.__class__(label) for label in self.split(";", maxsplit=1)[0].split("|"))
+        alternatives = self.split(self.HINT_SEP, maxsplit=1)[0].split(self.SPELLING_ALTERNATIVES_SEP)
+        return tuple(self.__class__(label) for label in alternatives)
 
     @property
     def hint(self) -> str:
         """Return the label hint, if any."""
-        return self.split(";")[1] if ";" in self else ""
+        return self.split(self.HINT_SEP)[1] if self.HINT_SEP in self else ""
 
 
 Labels = tuple[Label, ...]
