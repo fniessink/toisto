a989d0fa25d4f0b6e487e0ea4807c2d41265ab5e
Frank Niessink <frank@niessink.com>
2023-05-13 21:40:00 +0200

    Improve test coverage. (#344)

--- tests/toisto/ui/test_cli.py
+++ tests/toisto/ui/test_cli.py
@@ -7,6 +7,7 @@ from argparse import ArgumentParser, Namespace
 from configparser import ConfigParser
 from unittest.mock import Mock, patch
 
+from toisto.model.language.concept import Topic
 from toisto.ui.cli import create_argument_parser
 
 
@@ -19,17 +20,20 @@ class ParserTest(unittest.TestCase):
         """Set up the test fixtures."""
         os.environ["COLUMNS"] = "120"  # Set the width of the terminal to match the formatting of the expected results
 
-    @property
-    def argument_parser(self) -> ArgumentParser:
+    def argument_parser(
+        self,
+        config_parser: ConfigParser | None = None,
+        topics: list[Topic] | None = None,
+    ) -> ArgumentParser:
         """Create the argument parser."""
         with patch("requests.get", Mock(return_value=Mock(json=Mock(return_value=[dict(name="v9999")])))):
-            return create_argument_parser(ConfigParser(), [])
+            return create_argument_parser(config_parser or ConfigParser(), topics)
 
     @patch("sys.argv", ["toisto", "--help"])
     @patch("sys.stdout.write")
     def test_help(self, sys_stdout_write: Mock):
         """Test that the help message is displayed."""
-        self.assertRaises(SystemExit, self.argument_parser.parse_args)
+        self.assertRaises(SystemExit, self.argument_parser().parse_args)
         self.assertEqual(
             """Usage: toisto [-h] [-V] {practice,progress,topics} ...
 
@@ -64,13 +68,13 @@ See https://github.com/fniessink/toisto/blob/main/README.md for more information
             topic=[],
             topic_file=[],
         )
-        self.assertEqual(expected_namespace, self.argument_parser.parse_args())
+        self.assertEqual(expected_namespace, self.argument_parser().parse_args())
 
     @patch("sys.argv", ["toisto", "practice", "--help"])
     @patch("sys.stdout.write")
     def test_practice_help(self, sys_stdout_write: Mock):
         """Test that the practice help message is displayed."""
-        self.assertRaises(SystemExit, self.argument_parser.parse_args)
+        self.assertRaises(SystemExit, self.argument_parser(topics=[Topic("T1"), Topic("T2")]).parse_args)
         self.assertEqual(
             """Usage: toisto practice [-h] -t {language} -s {language} [-l {level}] [-T {topic}] [-f {topic file}]
 
@@ -84,6 +88,63 @@ Options:
                         source language; languages available in built-in topics: en, fi, nl
   -l, --level {level}   language levels to use, can be repeated; default: all; available levels: A1, A2, B1, B2, C1,
                         C2
+  -T, --topic {topic}   topic to use, can be repeated; default: all; built-in topics: T1, T2
+  -f, --topic-file {topic file}
+                        topic file to use, can be repeated
+""",
+            self.ANSI_ESCAPE_CODES.sub("", sys_stdout_write.call_args_list[2][0][0]),
+        )
+
+    @patch("sys.argv", ["toisto", "practice", "--help"])
+    @patch("sys.stdout.write")
+    def test_practice_help_with_default_languages_in_config(self, sys_stdout_write: Mock):
+        """Test that the practice help message is displayed."""
+        config_parser = ConfigParser()
+        config_parser.add_section("languages")
+        config_parser.set("languages", "target", "fi")
+        self.assertRaises(SystemExit, self.argument_parser(config_parser).parse_args)
+        self.assertEqual(
+            """Usage: toisto practice [-h] [-t {language}] -s {language} [-l {level}] [-T {topic}] [-f {topic file}]
+
+Practice a language.
+
+Options:
+  -h, --help            show this help message and exit
+  -t, --target {language}
+                        target language; default: fi; languages available in built-in topics: en, fi, nl
+  -s, --source {language}
+                        source language; languages available in built-in topics: en, fi, nl
+  -l, --level {level}   language levels to use, can be repeated; default: all; available levels: A1, A2, B1, B2, C1,
+                        C2
+  -T, --topic {topic}   topic to use, can be repeated; default: all; built-in topics:
+  -f, --topic-file {topic file}
+                        topic file to use, can be repeated
+""",
+            self.ANSI_ESCAPE_CODES.sub("", sys_stdout_write.call_args_list[2][0][0]),
+        )
+
+    @patch("sys.argv", ["toisto", "practice", "--help"])
+    @patch("sys.stdout.write")
+    def test_practice_help_with_default_levels_in_config(self, sys_stdout_write: Mock):
+        """Test that the practice help message is displayed."""
+        config_parser = ConfigParser()
+        config_parser.add_section("languages")
+        config_parser.set("languages", "levels", "A1 A2")
+        self.assertRaises(SystemExit, self.argument_parser(config_parser).parse_args)
+        self.maxDiff = None
+        self.assertEqual(
+            """Usage: toisto practice [-h] -t {language} -s {language} [-l {level}] [-T {topic}] [-f {topic file}]
+
+Practice a language.
+
+Options:
+  -h, --help            show this help message and exit
+  -t, --target {language}
+                        target language; languages available in built-in topics: en, fi, nl
+  -s, --source {language}
+                        source language; languages available in built-in topics: en, fi, nl
+  -l, --level {level}   language levels to use, can be repeated; default: A1, A2; available levels: A1, A2, B1, B2,
+                        C1, C2
   -T, --topic {topic}   topic to use, can be repeated; default: all; built-in topics:
   -f, --topic-file {topic file}
                         topic file to use, can be repeated
@@ -95,7 +156,7 @@ Options:
     @patch("sys.stdout.write")
     def test_progress_help(self, sys_stdout_write: Mock):
         """Test that the progress help message is displayed."""
-        self.assertRaises(SystemExit, self.argument_parser.parse_args)
+        self.assertRaises(SystemExit, self.argument_parser().parse_args)
         self.assertEqual(
             """Usage: toisto progress [-h] -t {language} -s {language} [-l {level}] [-T {topic}] [-f {topic file}] \
 [-S {option}]
@@ -123,7 +184,7 @@ Options:
     @patch("sys.stdout.write")
     def test_topics_help(self, sys_stdout_write: Mock):
         """Test that the topics help message is displayed."""
-        self.assertRaises(SystemExit, self.argument_parser.parse_args)
+        self.assertRaises(SystemExit, self.argument_parser().parse_args)
         self.assertEqual(
             """Usage: toisto topics [-h] -t {language} -s {language} [-l {level}] [-T {topic}] [-f {topic file}]
 
@@ -155,27 +216,27 @@ Options:
             topic=[],
             topic_file=[],
         )
-        self.assertEqual(expected_namespace, self.argument_parser.parse_args())
+        self.assertEqual(expected_namespace, self.argument_parser().parse_args())
 
     @patch("sys.argv", ["toisto", "--version"])
     @patch.object(ArgumentParser, "_print_message")
     def test_version_long_option(self, print_message: Mock):
         """Test that the app writes the version number to stdout."""
-        self.assertRaises(SystemExit, self.argument_parser.parse_args)
+        self.assertRaises(SystemExit, self.argument_parser().parse_args)
         self.assertRegex(print_message.call_args_list[0][0][0], r"\d+.\d+.\d+")
 
     @patch("sys.argv", ["toisto", "-V"])
     @patch.object(ArgumentParser, "_print_message")
     def test_version_short_option(self, print_message: Mock):
         """Test that the app writes the version number to stdout."""
-        self.assertRaises(SystemExit, self.argument_parser.parse_args)
+        self.assertRaises(SystemExit, self.argument_parser().parse_args)
         self.assertRegex(print_message.call_args_list[0][0][0], r"\d+.\d+.\d+")
 
     @patch("sys.argv", ["toisto", "practice", "--target", "42", "--source", "@@"])
     @patch("sys.stderr.write")
     def test_invalid_language(self, sys_stderr_write: Mock):
         """Test that an error message is displayed if an invalid language is supplied."""
-        self.assertRaises(SystemExit, self.argument_parser.parse_args)
+        self.assertRaises(SystemExit, self.argument_parser().parse_args)
         self.assertIn(
             "invalid choice: '42' (see https://www.iana.org/assignments/language-subtag-registry for valid choices)",
             sys_stderr_write.call_args_list[1][0][0],
