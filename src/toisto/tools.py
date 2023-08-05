8d2a1de9a1316b2601a31141705db0c7ea7d436e
Frank Niessink <frank@niessink.com>
2023-08-05 18:21:40 +0200

    Refactor how grammatical quizzes are generated.

--- src/toisto/tools.py
+++ src/toisto/tools.py
@@ -1,17 +1,9 @@
 """Utitity functions."""
 
-from collections.abc import Callable, Iterator, Sequence
-from itertools import cycle
+from collections.abc import Callable, Sequence
 from typing import TypeVar
 
 T = TypeVar("T")
-
-
-def zip_and_cycle(*lists: list[T]) -> Iterator[tuple[T]]:
-    """Zip the lists, while cycling lists that are shorter than the longest list."""
-    max_len = max(len(lst) for lst in lists) if lists else 0
-    cycled_lists = [cycle(lst) if len(lst) < max_len else lst for lst in lists]
-    yield from zip(*cycled_lists, strict=False)
 
 
 def first(sequence: Sequence[T], where: Callable[[T], bool] = lambda _item: True) -> T:
