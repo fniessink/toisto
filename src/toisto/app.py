cce99561cde99fc10cfe78fceefa298d8922fca7
Frank Niessink <frank@niessink.com>
2023-11-22 20:19:39 +0100

    Refactor main function. (#494)

--- src/toisto/app.py
+++ src/toisto/app.py
@@ -1,6 +1,8 @@
 """Main module for the application."""
 
 import logging
+from argparse import Namespace
+from configparser import ConfigParser
 from contextlib import suppress
 from pathlib import Path
 
@@ -15,7 +17,11 @@ from .command.show_progress import show_progress
 from .command.show_topics import show_topics
 from .metadata import CONCEPT_JSON_FILES, TOPIC_FILES, latest_version
 from .model.filter import filter_concepts
+from .model.language.concept import Concept
+from .model.quiz.progress import Progress
+from .model.quiz.quiz import Quizzes
 from .model.quiz.quiz_factory import create_quizzes
+from .model.topic.topic import Topic
 from .persistence.concepts import ConceptIdRegistry, load_concepts
 from .persistence.config import default_config, read_config
 from .persistence.progress import load_progress
@@ -24,8 +30,8 @@ from .ui.cli import create_argument_parser, parse_arguments
 from .ui.text import show_welcome
 
 
-def main() -> None:
-    """Run the main program."""
+def init() -> tuple[ConfigParser, Namespace, set[Concept], set[Topic], Quizzes, Progress]:
+    """Initialize the main program."""
     argument_parser = create_argument_parser(default_config())
     config = read_config(argument_parser)
     registry = ConceptIdRegistry(argument_parser)
@@ -40,6 +46,12 @@ def main() -> None:
     concepts = filter_concepts(concepts, topics, args.concept, args.topic, argument_parser)
     quizzes = create_quizzes(args.target_language, args.source_language, *concepts)
     progress = load_progress(args.target_language, argument_parser)
+    return config, args, concepts, topics, quizzes, progress
+
+
+def main() -> None:
+    """Run the main program."""
+    config, args, concepts, topics, quizzes, progress = init()
     match args.command:
         case "topics":
             show_topics(args.target_language, args.source_language, topics, concepts)
