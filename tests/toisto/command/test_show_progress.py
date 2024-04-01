5f75e010b3dfb980fb72dccb376c0e17157e209f
Frank Niessink <frank@niessink.com>
2024-04-01 22:04:12 +0200

    Fix sorting by retention order.
    The progress command would not sort the quizzes correctly when sorting by retention.
    
    Fixes #625.

--- tests/toisto/command/test_show_progress.py
+++ tests/toisto/command/test_show_progress.py
@@ -14,36 +14,45 @@ from toisto.tools import first
 from ...base import ToistoTestCase
 
 
-class ShowProgressTest(ToistoTestCase):
-    """Test the show progress command."""
+class ShowProgressTestCase(ToistoTestCase):
+    """Base class for unit tests for the show progress command."""
 
-    def setUp(self):
+    SORT_COLUMN: SortColumn = "attempts"
+
+    def setUp(self) -> None:
         """Set up test fixtures."""
         super().setUp()
-        self.concept = self.create_concept("hello", dict(fi="Terve!", nl="Hoi!"))
-        self.quizzes = create_quizzes(self.fi, self.nl, self.concept).by_quiz_type("read")
-        self.quiz = first(self.quizzes)
+        concept = self.create_concept("hello", dict(fi="Terve!", nl="Hoi!"))
+        self.quiz = first(create_quizzes(self.fi, self.nl, concept).by_quiz_type("read"))
+        self.quizzes = Quizzes({self.quiz})
 
     @patch("rich.console.Console.pager", MagicMock())
-    def show_progress(self, progress: Progress, sort: SortColumn = "attempts") -> Mock:
+    def show_progress(self, progress: Progress) -> Mock:
         """Run the show progress command."""
         with patch("rich.console.Console.print") as console_print:
-            show_progress(Language("fi"), progress, sort=sort)
+            show_progress(Language("fi"), progress, sort=self.SORT_COLUMN)
         return console_print
+
+
+class ShowProgressTest(ShowProgressTestCase):
+    """Test the show progress command."""
+
+    def setUp(self) -> None:
+        """Extend to set up retention."""
+        super().setUp()
+        self.now = datetime.now()
+        start = (self.now - timedelta(hours=1)).isoformat(timespec="seconds")
+        end = self.now.isoformat(timespec="seconds")
+        self.progress = Progress({self.quiz.key: dict(start=start, end=end)}, self.fi, self.quizzes)
 
     def test_title(self):
         """Test the table title."""
-        progress = Progress({}, self.fi, self.quizzes)
-        console_print = self.show_progress(progress)
+        console_print = self.show_progress(self.progress)
         self.assertEqual("Progress Finnish", console_print.call_args[0][0].title)
 
     def test_column_headers(self):
         """Test that the column headers are shown."""
-        now = datetime.now()
-        start = (now - timedelta(hours=1)).isoformat(timespec="seconds")
-        end = now.isoformat(timespec="seconds")
-        progress = Progress({self.quiz.key: dict(start=start, end=end)}, self.fi, self.quizzes)
-        console_print = self.show_progress(progress)
+        console_print = self.show_progress(self.progress)
         for index, value in enumerate(
             ["Quiz type", "Question", "From", "To", "Answer(s)", "Attempts", "Retention", "Not quizzed until"],
         ):
@@ -51,40 +60,93 @@ class ShowProgressTest(ToistoTestCase):
 
     def test_quiz(self):
         """Test that quizzes are shown."""
-        now = datetime.now()
-        start = (now - timedelta(hours=1)).isoformat(timespec="seconds")
-        end = now.isoformat(timespec="seconds")
-        progress = Progress({self.quiz.key: dict(start=start, end=end)}, self.fi, self.quizzes)
-        console_print = self.show_progress(progress)
+        console_print = self.show_progress(self.progress)
         for index, value in enumerate(["Read", Label(self.fi, "Terve!"), "fi", "nl", "Hoi!", "0", "60 minutes", ""]):
             self.assertEqual(value, first(console_print.call_args[0][0].columns[index].cells))
 
     def test_quiz_silenced_until_time_in_the_future(self):
         """Test that if the time until which a quiz is silenced lies in the future, it is shown."""
-        skip_until = (datetime.now() + timedelta(days=1)).isoformat(sep=" ", timespec="minutes")
-        progress = Progress({self.quiz.key: dict(skip_until=skip_until)}, self.fi, self.quizzes)
-        console_print = self.show_progress(progress)
-        self.assertEqual(skip_until, first(console_print.call_args[0][0].columns[7].cells))
+        skip_until = self.progress.get_retention(self.quiz).skip_until = self.now + timedelta(days=1)
+        formatted_skip_until = skip_until.isoformat(sep=" ", timespec="minutes")
+        console_print = self.show_progress(self.progress)
+        self.assertEqual(formatted_skip_until, first(console_print.call_args[0][0].columns[7].cells))
 
     def test_quiz_silenced_until_time_in_the_past(self):
         """Test that if the time until which a quiz is silenced lies in the past, it is not shown."""
-        skip_until = (datetime.now() - timedelta(days=1)).isoformat(sep=" ", timespec="minutes")
-        progress = Progress({self.quiz.key: dict(skip_until=skip_until)}, self.fi, self.quizzes)
-        console_print = self.show_progress(progress)
+        self.progress.get_retention(self.quiz).skip_until = self.now - timedelta(days=1)
+        console_print = self.show_progress(self.progress)
         self.assertEqual("", first(console_print.call_args[0][0].columns[7].cells))
 
-    def test_sort_by_retention(self):
-        """Test that the quizzes can be sorted by retention length."""
-        now = datetime.now()
-        start = (now - timedelta(hours=1)).isoformat(timespec="seconds")
-        end = now.isoformat(timespec="seconds")
+
+class ShowProgressSortTestCase(ShowProgressTestCase):
+    """Base class for unit tests that test the sorting of the show progress command."""
+
+    def setUp(self) -> None:
+        """Set up test fixtures."""
+        super().setUp()
         another_concept = self.create_concept("carpet", dict(fi="matto", nl="het tapijt"))
-        another_quiz = create_quizzes(self.fi, self.nl, another_concept).by_quiz_type("read").pop()
-        quizzes = Quizzes({self.quiz, another_quiz})
+        self.another_quiz = first(create_quizzes(self.fi, self.nl, another_concept).by_quiz_type("read"))
+        self.quizzes = Quizzes({self.quiz, self.another_quiz})
+
+
+class ShowProgressByAttemptsTest(ShowProgressSortTestCase):
+    """Test the show progress command, when sorting by atrempts."""
+
+    SORT_COLUMN = "attempts"
+
+    def test_sort_by_attempts_when_one_quiz_has_no_attempts(self):
+        """Test that the quizzes can be sorted by retention length."""
         progress = Progress(
-            {self.quiz.key: dict(count=21, start=start, end=end), another_quiz.key: dict(count=42)},
+            {self.quiz.key: dict(count=21), self.another_quiz.key: {}},
             self.fi,
-            quizzes,
+            self.quizzes,
         )
-        console_print = self.show_progress(progress, sort="retention")
-        self.assertEqual(["21", "42"], list(console_print.call_args[0][0].columns[5].cells))
+        console_print = self.show_progress(progress)
+        self.assertEqual(["21", "0"], list(console_print.call_args[0][0].columns[5].cells))
+
+    def test_sort_by_attempts_numerically(self):
+        """Test that the quizzes are sorted by attempts numerically, not lexically."""
+        progress = Progress(
+            {self.quiz.key: dict(count=21), self.another_quiz.key: dict(count=4)},
+            self.fi,
+            self.quizzes,
+        )
+        console_print = self.show_progress(progress)
+        self.assertEqual(["21", "4"], list(console_print.call_args[0][0].columns[5].cells))
+
+
+class ShowProgressByRetentionTest(ShowProgressSortTestCase):
+    """Test the show progress command, when sorting by retention."""
+
+    SORT_COLUMN = "retention"
+
+    def setUp(self) -> None:
+        """Extend to set up retentions."""
+        super().setUp()
+        self.now = datetime.now()
+        self.start = (self.now - timedelta(days=88)).isoformat(timespec="seconds")
+        self.end = self.now.isoformat(timespec="seconds")
+
+    def test_sort_by_retention_when_one_quiz_has_no_retention(self):
+        """Test that the quizzes can be sorted by retention length, even when a quiz has no retention."""
+        progress = Progress(
+            {self.quiz.key: dict(start=self.start, end=self.end), self.another_quiz.key: {}},
+            self.fi,
+            self.quizzes,
+        )
+        console_print = self.show_progress(progress)
+        self.assertEqual(["88 days", ""], list(console_print.call_args[0][0].columns[6].cells))
+
+    def test_sort_by_retention_numerically(self):
+        """Test that the quizzes are sorted by retention length numerically, not lexically."""
+        another_start = (self.now - timedelta(days=9)).isoformat(timespec="seconds")
+        progress = Progress(
+            {
+                self.quiz.key: dict(start=self.start, end=self.end),
+                self.another_quiz.key: dict(start=another_start, end=self.end),
+            },
+            self.fi,
+            self.quizzes,
+        )
+        console_print = self.show_progress(progress)
+        self.assertEqual(["88 days", "9 days"], list(console_print.call_args[0][0].columns[6].cells))
