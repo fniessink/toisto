cedeceee8fe8b1cb22b8cfd2a0cd87c441ca3078
Frank Niessink <frank@niessink.com>
2024-10-20 23:19:27 +0200

    Don't use concept id in quiz repr.
    Don't use concept id in quiz repr, but instead use question and answer so that progress doesn't get lost when the concept id changes.

--- src/toisto/model/quiz/quiz.py
+++ src/toisto/model/quiz/quiz.py
@@ -48,9 +48,8 @@ class Quiz:
     @cached_property
     def key(self) -> str:
         """Return a string version of the quiz that can be used as key in the progress dict."""
-        concept_id = self.concept.base_concept.concept_id
         question = self._question.first_spelling_alternative
-        return f"{concept_id}:{self.question.language}:{self.answer.language}:{question}:{self.quiz_type.action}"
+        return f"{self.question.language}:{self.answer.language}:{question}:{self.answer}:{self.quiz_type.action}"
 
     def has_quiz_type(self, quiz_type: QuizType | type[QuizType]) -> bool:
         """Return whether this quiz has the specified quiz type."""
